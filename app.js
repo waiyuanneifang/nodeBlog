d804e983e70ff9de1006df0f42fa3f8db7573e4c
waiyuanneifang <yifanli@YifanLideMacBook-Pro.local>
2019-07-02 17:28:58 +0800

    1

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
+const handleBlogRouter = require("./src/router/blog.js")
+const handleUserRouter = require("./src/router/user.js")
+
 const serverHandle = (req, res) => {
-	res.setHeader("Content-type", "application/json")
+	res.setHeader("Content-Type", "application/json")
+
+	// 处理bolg路由
+	const blogData = handleBlogRouter(req, res)
+	if (blogData) {
+		res.end(JSON.stringify(blogData))
+		return
+	}
+
+	// 处理user路由
+	const userData = handleUserRouter(req, res)
+	if (userData) {
+		res.end(JSON.stringify(userData))
+		return
+	}
+
+	// 未命中路由返回404
+	res.writeHead(404, { "Content-Type": "text/plain" })
+	res.write("404 Not Found\n")
+	res.end()
 }
 
 module.exports = serverHandle
--- src/router/blog.js
+++ src/router/blog.js
@@ -9,9 +9,29 @@ const handleBlogRouter = (req, res) => {
 		}
 	}
 
-	if (method === "GET" && path === "/api") {
+	if (method === "GET" && path === "/api/blog/detail") {
 		return {
 			msg: "获取博客详情"
 		}
 	}
+
+	if (method === "POST" && path === "/api/blog/updata") {
+		return {
+			msg: "新建博客"
+		}
+	}
+
+	if (method === "POST" && path === "/api/blog/updata") {
+		return {
+			msg: "更新博客"
+		}
+	}
+
+	if (method === "POST" && path === "/api/blog/del") {
+		return {
+			msg: "删除博客"
+		}
+	}
 }
+
+module.exports = handleBlogRouter
