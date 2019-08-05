41bafd8933f7c951ab32a5a3f9f780f3438c4394
waiyuanneifang <yifanli@172.16.251.150>
2019-08-05 17:13:57 +0800

    1

--- src/controller/user.js
+++ src/controller/user.js
@@ -2,7 +2,7 @@ const { exec } = require("../db/mysql.js")
 
 const login = (username, password) => {
 	const sql = `
-		select username, realname from users where username='${username}' and password='${password}'
+		select id, username, realname from users where username='${username}' and password='${password}'
 	`
 	return exec(sql).then(rows => {
 		return rows[0]
--- src/router/user.js
+++ src/router/user.js
@@ -1,5 +1,6 @@
 const { login } = require("../controller/user.js")
 const { SuccessModel, ErrorModel } = require("../model/resModel.js")
+const { set } = require("../db/redis.js")
 
 const handleUserRouter = (req, res) => {
 	const method = req.method
@@ -9,22 +10,8 @@ const handleUserRouter = (req, res) => {
 		const result = login(username, password)
 		return result.then(data => {
 			if (data) {
+				set(data.id, { username, password })
 				return new SuccessModel(data)
-			}
-			return new ErrorModel("登录失败")
-		})
-	}
-
-	if (method === "GET" && req.path === "/api/user/test-login") {
-		const { username, password } = req.query
-		const result = login(username, password)
-		return result.then(data => {
-			if (data) {
-				req.session.username = data.username
-				req.session.realname = data.realname
-				return new SuccessModel({
-					session: req.session
-				})
 			}
 			return new ErrorModel("登录失败")
 		})
