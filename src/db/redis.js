5eaa61a6a953ab8afd8eed923d425f73f62b8fee
waiyuanneifang <yifanli@172.16.251.150>
2019-08-12 10:28:55 +0800

    1

--- src/db/redis.js
+++ src/db/redis.js
@@ -1,40 +1,38 @@
-const redis = require('redis')
-const { REDIS_CONF } from '../conf.js'
+const redis = require("redis")
+const { REDIS_CONF } = require("../conf/db.js")
 
 const redisClient = redis.createClient(REDIS_CONF.port, REDIS_CONF.host)
-redisClient.on('error', err => {
-    console.log(err)
+redisClient.on("error", err => {
+	console.log(err)
 })
 
-function set (key, val) {
-    if (typeof val === 'object') {
-        val = JSON.stringify(val)
-    }
-    redisClient.set(key, val, redis.print)
+function set(key, val) {
+	if (typeof val === "object") {
+		val = JSON.stringify(val)
+	}
+	redisClient.set(key, val, redis.print)
 }
-function get (key) {
-    return new Promise((resolve, reject) => {
-        redisClient.get(key, (err, val) => {
-            if (err) {
-                reject(err)
-                return 
-            }
-            if (val === null) {
-                resolve(null)
-                return
-            }
-            try {
-                resolve(
-                    JSON.parse(val)
-                ) 
-            } catch (ex) {
-                resolve(val)
-            }
-        })
-    })
+function get(key) {
+	return new Promise((resolve, reject) => {
+		redisClient.get(key, (err, val) => {
+			if (err) {
+				reject(err)
+				return
+			}
+			if (val === null) {
+				resolve(null)
+				return
+			}
+			try {
+				resolve(JSON.parse(val))
+			} catch (ex) {
+				resolve(val)
+			}
+		})
+	})
 }
 
 module.exports = {
-    set,
-    get
+	set,
+	get
 }
