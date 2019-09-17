75d9b258cf62d184c3d58c89d6fbf8dee1878c51
waiyuanneifang <yifanli@172.16.251.160>
2019-09-17 11:07:36 +0800

    1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const querystring = require("querystring")
 const handleBlogRouter = require("./src/router/blog.js")
 const handleUserRouter = require("./src/router/user.js")
 const { access } = require("./src//utils/log.js")
-const { get } = require("./src/db/redis.js")
+const { get, set } = require("./src/db/redis.js")
 
 const getCoolieExpires = () => {
 	const d = new Date()
@@ -59,57 +59,61 @@ const serverHandle = (req, res) => {
 	})
 
 	// 解析session
+	let needSetCookie = false
 	let userId = req.cookie.userid
-	req.session = {}
+	if (!userId) {
+		needSetCookie = true
+		userId = `${Date.now()}_${Math.random()}`
+		set(userId, {})
+	}
+	req.sessionId = userId
+	get(req.sessionId)
+		.then(sessionData => {
+			if (sessionData == null) {
+				set(req.sessionId, {})
+				req.session = {}
+			} else {
+				req.session = sessionData
+			}
+			return getPostData(req)
+		})
+		.then(postData => {
+			req.body = postData
+			// 处理bolg路由
+			const blogResult = handleBlogRouter(req, res)
+			if (blogResult) {
+				blogResult.then(blogData => {
+					if (needSetCookie) {
+						res.setHeader(
+							"Set-Cookie",
+							`userid=${userId}; path=/; httpOnly; expires=${getCoolieExpires()}`
+						)
+					}
+					res.end(JSON.stringify(blogData))
+				})
+				return
+			}
 
-	Promise.all([get(userId), getPostData(req)]).then(results => {
-		if (userId) {
-			req.session = results[0]
-		}
-		req.body = results[1]
-		// 处理bolg路由
-		const blogResult = handleBlogRouter(req, res)
-		if (blogResult) {
-			blogResult.then(blogData => {
-				if (userId) {
-					res.setHeader(
-						"Set-Cookie",
-						`userid=${userId}; path=/; httpOnly; expires=${getCoolieExpires()}`
-					)
-				}
-				res.end(JSON.stringify(blogData))
-			})
-			return
-		}
+			// 处理user路由
+			const userResult = handleUserRouter(req, res)
+			if (userResult) {
+				userResult.then(userData => {
+					if (needSetCookie) {
+						res.setHeader(
+							"Set-Cookie",
+							`userid=${userId}; path=/; httpOnly; expires=${getCoolieExpires()}`
+						)
+					}
+					res.end(JSON.stringify(userData))
+				})
+				return
+			}
 
-		// 处理user路由
-		const userResult = handleUserRouter(req, res)
-		if (userResult) {
-			userResult.then(userData => {
-				if (userId) {
-					res.setHeader(
-						"Set-Cookie",
-						`userid=${userId}; path=/; httpOnly; expires=${getCoolieExpires()}`
-					)
-				}
-				if (userData.data.id) {
-					res.setHeader(
-						"Set-Cookie",
-						`userid=${
-							userData.data.id
-						}; path=/; httpOnly; expires=${getCoolieExpires()}`
-					)
-				}
-				res.end(JSON.stringify(userData))
-			})
-			return
-		}
-
-		// 未命中路由返回404
-		res.writeHead(404, { "Content-type": "text/plain" })
-		res.write("404 Not Found\n")
-		res.end()
-	})
+			// 未命中路由返回404
+			res.writeHead(404, { "Content-type": "text/plain" })
+			res.write("404 Not Found\n")
+			res.end()
+		})
 }
 
 module.exports = serverHandle
